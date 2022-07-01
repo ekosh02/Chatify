4a6cd06c6a3b44ee5ec5ea0c71abad31628b1c30
ekosh02 <turapbaeveldos@gmail.com>
2022-07-01 17:56:10 +0600

    optFlatlist

--- src/screens/postScreens/PostScreen.js
+++ src/screens/postScreens/PostScreen.js
@@ -1,75 +1,67 @@
-import {
-  View,
-  FlatList,
-  Text,
-  TouchableOpacity
-} from 'react-native'
-import React, { useEffect, useState } from 'react'
+import {View, FlatList, Text, TouchableOpacity} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
 
-import { getProject } from './../../func/getApi'
+import {getProject} from './../../func/getApi';
 
-import Actions from './../../styles/Actions'
-import { styles } from './../../styles/AppBarAndList'
+import Actions from './../../styles/Actions';
+import {styles} from './../../styles/AppBarAndList';
 
-import {Indicator} from './../../styles/ActivityIndicator'
+import {Indicator} from './../../styles/ActivityIndicator';
 
 import {strings} from '../../Localization/Localization';
 
 export function PostScreen(props) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
 
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState(null)
-
-  const api = 'posts'
+  const api = 'posts';
 
   useEffect(() => {
-    getProject(setData, setLoading, api)
+    getProject(setData, setLoading, api);
   }, []);
 
   return (
     <View style={styles.container}>
-     <Actions text={strings.post}/>
-      
+      <Actions text={strings.post} />
+
       {chatList(loading, data, props)}
     </View>
   );
-
 }
 
 function chatList(loading, data, props) {
+  const renderProject = useCallback(
+    ({item}) => (
+      <View style={styles.shell}>
+        <TouchableOpacity
+          onPress={() =>
+            props.navigation.navigate('PostScreenDetails', params)
+          }>
+          <Text style={styles.nameTextStyle} numberOfLines={1}>
+            {item.title}
+          </Text>
+          <Text style={styles.descriptionTextStyle} numberOfLines={1}>
+            {item.body}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    ),
+    [],
+  );
 
   return (
-  <View style={styles.listContainer}>
-    {loading ? (
-      <Indicator />
-    ) : (
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <RenderProject item={item} props={props} />
-        )}
-        contentContainerStyle={styles.contentContainerStyle} />
-    )}
-  </View>);
-}
-const RenderProject = ({ item, props }) => {
-
-  let params = {
-    title: item.title,
-    body: item.body,
-    userId: item.userId,
-    id: item.id
-  };
-
-  return (
-    <View style={styles.shell}>
-      <TouchableOpacity onPress={() => props.navigation.navigate('PostScreenDetails', params)}>
-        <Text style={styles.nameTextStyle} numberOfLines={1}>{item.title}</Text>
-        <Text style={styles.descriptionTextStyle} numberOfLines={1}>{item.body}</Text>
-      </TouchableOpacity>
+    <View style={styles.listContainer}>
+      {loading ? (
+        <Indicator />
+      ) : (
+        <FlatList
+          maxToRenderPerBatch={1}
+          initialNumToRender={14}
+          data={data}
+          renderItem={renderProject}
+          contentContainerStyle={styles.contentContainerStyle}
+        />
+      )}
     </View>
   );
-};
-
-
-
+}
